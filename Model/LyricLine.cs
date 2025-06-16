753ea1d977789974144c9558341df3293ba443d6
pkc <906847109@qq.com>
2025-06-16 18:36:07 +0800

    MVVM

--- Model/LyricLine.cs
+++ Model/LyricLine.cs
@@ -1,47 +1,50 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace WinUIMusicPlayer.Model
 {
-    public class LyricLine : INotifyPropertyChanged
+    public partial class LyricLine : ObservableObject
     {
+        [ObservableProperty]
         private string _text;
+        [ObservableProperty]
         private bool _isCurrent;
 
-        public string Text
-        {
-            get => _text;
-            set
-            {
-                if (_text != value)
-                {
-                    _text = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+        //public string Text
+        //{
+        //    get => _text;
+        //    set
+        //    {
+        //        if (_text != value)
+        //        {
+        //            _text = value;
+        //            OnPropertyChanged();
+        //        }
+        //    }
+        //}
 
         public TimeSpan Time { get; set; }
 
-        public bool IsCurrent
-        {
-            get => _isCurrent;
-            set
-            {
-                if (_isCurrent != value)
-                {
-                    _isCurrent = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+        //public bool IsCurrent
+        //{
+        //    get => _isCurrent;
+        //    set
+        //    {
+        //        if (_isCurrent != value)
+        //        {
+        //            _isCurrent = value;
+        //            OnPropertyChanged();
+        //        }
+        //    }
+        //}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        //protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
