66a1a19db687417a9395393680371d39f4619583
pkc <906847109@qq.com>
2025-07-08 17:12:02 +0800

    4种格式

--- Utils/ToolUtils.cs
+++ Utils/ToolUtils.cs
@@ -603,7 +603,7 @@ namespace WinUIMusicPlayer.Utils
 
         public static bool IsMusicFile(string fileType)
         {
-            var musicExtensions = new[] { ".mp3", ".wav", ".flac", ".wma", ".aac", ".ogg", ".aiff", ".m4a", ".dsf", ".dff" };
+            var musicExtensions = new[] { ".mp3", ".wav", ".flac", ".wma", ".aac", ".ogg", ".aiff",".aif", ".m4a", ".dsf", ".dff", ".amr" ,".au",".ac3",".ape"};
             return musicExtensions.Contains(fileType.ToLower());
         }
 
