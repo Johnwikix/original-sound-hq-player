3322ebfa665c655a419607776aa52a1ec5f2d47d
pkc <906847109@qq.com>
2025-07-08 17:24:24 +0800

    格式转换trycatch

--- Services/AudioConverterService.cs
+++ Services/AudioConverterService.cs
@@ -19,64 +19,70 @@ namespace WinUIMusicPlayer.Services
             };
         }
         public async Task ConvertAudio2Wav(Music music, string type = "wav")
-        {            
-            string outputPath = converter.GenerateOutputPath(music.Path, type);
-            switch (music.Extension.ToLower())
+        {
+            try
             {
-                case "mp3":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertMp3(music.Path, outputPath, type);
-                    });
-                    break;
-                case "flac":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertFlac(music.Path, outputPath, type);
-                    });
-                    break;
-                case "aiff":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertAiff(music.Path, outputPath, type);
-                    });
-                    break;
-                case "aif":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertAiff(music.Path, outputPath, type);
-                    });
-                    break;
-                case "wav":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertWav(music.Path, outputPath, type);
-                    });
-                    break;
-                case "ogg":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertOgg(music.Path, outputPath, type);
-                    });
-                    break;
-                case "dsf":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertDSDToWav(music.Path, outputPath, type);
-                    });
-                    break;
-                case "dff":
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertDSDToWav(music.Path, outputPath, type);
-                    });
-                    break;
-                default:
-                    await Task.Run(() =>
-                    {
-                        converter.ConvertAudio(music.Path, outputPath, type);
-                    });
-                    break;
+                string outputPath = converter.GenerateOutputPath(music.Path, type);
+                switch (music.Extension.ToLower())
+                {
+                    case "mp3":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertMp3(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "flac":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertFlac(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "aiff":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertAiff(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "aif":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertAiff(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "wav":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertWav(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "ogg":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertOgg(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "dsf":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertDSDToWav(music.Path, outputPath, type);
+                        });
+                        break;
+                    case "dff":
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertDSDToWav(music.Path, outputPath, type);
+                        });
+                        break;
+                    default:
+                        await Task.Run(() =>
+                        {
+                            converter.ConvertAudio(music.Path, outputPath, type);
+                        });
+                        break;
+                }
+            }
+            catch (Exception e) {
+                OnProgressChanged(100);
             }
         }
 
