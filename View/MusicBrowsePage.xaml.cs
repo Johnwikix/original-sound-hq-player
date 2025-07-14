db2baa12cd951a21cf142f84840db778976d62e0
pkc <906847109@qq.com>
2025-07-14 17:09:43 +0800

    样式更改

--- View/MusicBrowsePage.xaml.cs
+++ View/MusicBrowsePage.xaml.cs
@@ -137,11 +137,12 @@ namespace WinUIMusicPlayer.View
         }
 
         public void SetAcrylicBrushBackground() {
-            var acrylicBrush = new AcrylicBrush { TintOpacity = 0.7 };
+            var acrylicBrush = new AcrylicBrush { TintOpacity = 0.8 };
 
             if (((FrameworkElement)App.MainWindow!.Content).ActualTheme == ElementTheme.Dark)
             {
                 acrylicBrush.TintColor = Colors.Black;
+                acrylicBrush.TintOpacity = 0.5;
             }
             else
             {
