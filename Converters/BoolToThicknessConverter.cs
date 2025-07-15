8c905eec869889943c44c9700093ee945499d924
pkc <906847109@qq.com>
2025-07-15 10:29:18 +0800

    margin绑定

---
+++ Converters/BoolToThicknessConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUIMusicPlayer.Converters
+{
+    public class BoolToThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool playingDetail) {
+                return playingDetail ? new Thickness(0,0,0,0) : new Thickness(0, 32, 0, 0);
+            }
+            return new Thickness(0, 32, 0, 0); // Default value if not a boolean
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
