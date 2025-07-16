981c5ba870946e776c6802d36a4a956b28728232
pkc <906847109@qq.com>
2025-07-16 17:19:00 +0800

    bugfix

--- Model/AppData.cs
+++ Model/AppData.cs
@@ -23,7 +23,7 @@ namespace WinUIMusicPlayer.Model
         public static ObservableCollection<UsbStorageDevice> usbStorageDevices = new ObservableCollection<UsbStorageDevice>();
         public static UsbStorageDevice usbStorageDevice = new UsbStorageDevice();
         public static string sortOrder { get; set; } = "DefaultOrder";
-        public static IntPtr m_hWnd { get; set; } = IntPtr.Zero
+        public static IntPtr m_hWnd { get; set; } = IntPtr.Zero;
         public static double AppDpiScale { get; set; } = 1.0;
             //public static PlayMode currentPlayMode = PlayMode.ListLoop;
         }
