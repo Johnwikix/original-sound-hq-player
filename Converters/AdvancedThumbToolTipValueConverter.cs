b16138cdc514be1f1460a098c45f75eb1b2442eb
pkc <906847109@qq.com>
2025-07-28 16:43:51 +0800

    进度条提示改成分钟

---
+++ Converters/AdvancedThumbToolTipValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUIMusicPlayer.Converters
+{
+    public class AdvancedThumbToolTipValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double totalSeconds)
+            {
+                var timeSpan = TimeSpan.FromSeconds(totalSeconds);
+                if (timeSpan.TotalHours >= 1)
+                {
+                    return $"{(int)timeSpan.TotalHours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+                }
+                else
+                {
+                    return $"{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+                }
+            }
+            return "00:00";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
