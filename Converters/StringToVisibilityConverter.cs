12108cd1692388c8a6c1f19e48ece26a74d6fb1b
pkc <906847109@qq.com>
2026-01-17 20:33:45 +0800

    update

---
+++ Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinUIMusicPlayer.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string transLrc) {
+                if (string.IsNullOrEmpty(transLrc)) { 
+                    return Visibility.Collapsed;
+                }
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
