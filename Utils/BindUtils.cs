be82a4812d109440adccef7b6118e8fb18f2c706
pkc <906847109@qq.com>
2026-02-02 22:07:00 +0800

    使用静态转换器

---
+++ Utils/BindUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace WinUIMusicPlayer.Utils
+{
+    public static class BindUtils
+    {
+        public static bool RadioButtonTagToBoolConverter(string themeType,string type) {
+            if (themeType is null || type is null)
+                return false;
+            return themeType.Equals(type, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
