d5d9acc767448a95ffc96fea454c994ab39f6f03
pkc <906847109@qq.com>
2026-02-12 10:40:08 +0800

    添加右键菜单绑定扩展

---
+++ Extensions/MenuFlyoutExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WinUIMusicPlayer.Model;
+
+namespace WinUIMusicPlayer.Extensions
+{
+    public static class MenuFlyoutExtensions
+    {
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.RegisterAttached("ItemsSource", typeof(IEnumerable<MenuModel>),
+            typeof(MenuFlyoutExtensions), new PropertyMetadata(null, OnItemsSourceChanged));
+
+        public static void SetItemsSource(DependencyObject d, IEnumerable<MenuModel> value) => d.SetValue(ItemsSourceProperty, value);
+        public static IEnumerable<MenuModel> GetItemsSource(DependencyObject d) => (IEnumerable<MenuModel>)d.GetValue(ItemsSourceProperty);
+
+        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MenuFlyout flyout)
+            {
+                flyout.Items.Clear();
+                if (e.NewValue is IEnumerable<MenuModel> items)
+                {
+                    foreach (var item in items)
+                    {
+                        flyout.Items.Add(CreateMenuItem(item));
+                    }
+                }
+            }
+        }
+
+        private static MenuFlyoutItemBase CreateMenuItem(MenuModel model)
+        {
+            if (model.Children != null && model.Children.Count() > 0)
+            {
+                var subItem = new MenuFlyoutSubItem { Text = model.Title }; // 这里可以用资源加载器处理 Uid
+                foreach (var child in model.Children)
+                {
+                    subItem.Items.Add(CreateMenuItem(child));
+                }
+                return subItem;
+            }
+
+            var menuItem = new MenuFlyoutItem
+            {
+                Text = model.Title,
+                Command = model.Command,
+                CommandParameter = model.Tag
+            };
+            return menuItem;
+        }
+    }
+}
---
+++ Model/MenuModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace WinUIMusicPlayer.Model
+{
+    public class MenuModel
+    {
+        public string Title { get; set; }     // 对应 x:Uid 或 Text
+        public string Glyph { get; set; }     // 图标（可选）
+        public object Tag { get; set; }       // 存放类似 "wav", "mp3" 的参数
+        public ICommand Command { get; set; } // 绑定的事件
+        public IEnumerable<MenuModel> Children { get; set; } // 子菜单
+    }
+}
