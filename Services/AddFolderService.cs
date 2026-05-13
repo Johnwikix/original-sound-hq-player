fe889cbf5abf8fedb07e9a0ce1ab1df9b4db6f13
pkc <906847109@qq.com>
2026-05-13 17:53:58 +0800

    补充日志

--- Services/AddFolderService.cs
+++ Services/AddFolderService.cs
@@ -54,7 +54,7 @@ namespace WinUIMusicPlayer.Services
             {
                 // 即使提取元数据失败，也尝试添加基本信息
                 try
-                {
+                {_logger.LogError(ex, "
                     var music = new UsbDeviceMusic
                     {
                         Path = file.Path,
